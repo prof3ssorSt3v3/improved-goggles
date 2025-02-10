b3010b2d3f78dbb1a645bf982aab57d97ca20a02
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2025-02-10 11:46:37 -0500

    add Vercel link to home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//github.com/prof3ssorSt3v3/improved-goggles
+//prof3ssorSt3v3.github.io/improved-goggles/index.html
+//127.0.0.1:5500/index.html
+// https://improved-goggles-beryl.vercel.app/
+
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
